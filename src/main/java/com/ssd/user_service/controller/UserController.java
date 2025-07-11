c70b2c594a97e982c62ed7622319a1eec6dccb84
saurabh-dhote-git-hub <saurabhdhotegithub@gmail.com>
2025-07-12 01:51:14 +0530

    Initial commit

--- src/main/java/com/ssd/user_service/controller/UserController.java
+++ src/main/java/com/ssd/user_service/controller/UserController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
