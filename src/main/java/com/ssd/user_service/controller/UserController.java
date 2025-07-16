6d765683989a125d2767c8f99c8f72e6d84d2a7d
saurabh-dhote-git-hub <saurabhdhotegithub@gmail.com>
2025-07-16 19:05:21 +0530

    update application properties, downgrade Spring Boot version, and add Eureka client dependency; enhance UserController with logging

--- src/main/java/com/ssd/user_service/controller/UserController.java
+++ src/main/java/com/ssd/user_service/controller/UserController.java
@@ -7,6 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.UnknownHostException;
+import java.time.LocalDateTime;
+
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
@@ -14,14 +17,18 @@ public class UserController {
 
     private final UserService userService;
 
+    private int count = 1;
+
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> registerUser(@RequestBody UserRegistrationRequest request) {
+    public ResponseEntity<UserDTO> registerUser(@RequestBody UserRegistrationRequest request) throws UnknownHostException {
+        System.out.printf("Call: %d - %s \n", count++, LocalDateTime.now());
         UserDTO createdUser = userService.register(request);
         return ResponseEntity.ok(createdUser);
     }
 
     @GetMapping("/{username}")
-    public ResponseEntity<UserDTO> getUserByUsername(@PathVariable String username) {
+    public ResponseEntity<UserDTO> getUserByUsername(@PathVariable String username) throws UnknownHostException {
+        System.out.printf("Call: %d - %s \n", count++, LocalDateTime.now());
         UserDTO user = userService.getUser(username);
         return ResponseEntity.ok(user);
     }
